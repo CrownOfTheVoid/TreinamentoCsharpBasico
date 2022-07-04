fdfa1cad3ffbeef9b1f385655669b5d810d1585c
Aluno <felipe.m.martins@aluno.senai.br>
2022-07-04 11:23:41 -0300

    CS

---
+++ CursoGitHub.interfaceConsole/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System; 
+
+namespace CursoGitHub.interfaceConsole
+{
+    internal class Program
+    {
+   
+            static void Main(string[] args)
+
+
+            {
+                Console.WriteLine("Genshin");
+                Console.ReadKey();
+            }
+        }
+    }
+
